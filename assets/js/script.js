86bbf72ffa220e8461e21a42f6c7171dc7ad9e10
AntoUXUI <antoniouxjob@gmail.com>
2025-01-13 11:41:19 +0100

    Onclick_removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,19 @@ document.addEventListener("DOMContentLoaded", function () {
 document.getElementById('downloadCV').onclick = function() {
     window.open('./assets/pdf/CV-Antonio.pdf', '_blank');
 };
+
+document.getElementById('contactInfo').onclick = function() {
+    location.href = '#contact';
+};
+
+document.getElementById('linkedinProfile').onclick = function() {
+    window.open('https://linkedin.com/in/antonio-d-addio-a09b61257/', '_blank');
+};
+
+document.getElementById('linkedinProfile').onclick = function() {
+    window.open('https://linkedin.com/in/antonio-d-addio-a09b61257/', '_blank');
+};
+
+document.getElementById('githubProfile').onclick = function() {
+    window.open('https://github.com/AntoUXUI', '_blank');
+};
