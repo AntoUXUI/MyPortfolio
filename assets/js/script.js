2bf33bed771d339e44ddc0637948720f7f54ee56
AntoUXUI <antoniouxjob@gmail.com>
2025-01-23 17:33:16 +0100

    menu corretto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,3 @@
-function toggleMenu() {
- const menu = document.querySelector(".menu-links")
- const icon = document.querySelector(".hamburger-icon")
- menu.classList.toggle("open")
- icon.classList.toggle("open")
-}
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburgerIcon = document.querySelector(".hamburger-icon");
-    const menuLinks = document.querySelector(".menu-links");
-
-    // Aggiunge un listener al clic sull'icona dell'hamburger
-    hamburgerIcon.addEventListener("click", function () {
-        // Alterna la classe "open" per mostrare/nascondere il menu
-        menuLinks.classList.toggle("open");
-        hamburgerIcon.classList.toggle("open");
-    });
-});
-
 document.getElementById('downloadCV').onclick = function() {
     window.open('./assets/pdf/CV-Antonio.pdf', '_blank');
 };
